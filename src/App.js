418c8aacea564b138f81b2e861ed3c80f188eb9b
Amir Shevat <amir@reshuffle.com>
2019-11-18 17:25:37 -0800

    removed import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Admin from './components/Admin/Admin';
-import LiveFrame from './components/LiveFrame';
 import './App.css';
 
 function App() {
